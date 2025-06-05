11985464b8d6bd67b12b8cafea6db2a6032eb4bd
Piga Alessi, Esteban (Esteban Piga) <esteban.piga.alessi@one.verizon.com>
2025-06-05 17:27:31 +0000

    Vds Tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { MantineProvider } from '@mantine/core'
+import { useState } from 'react'
 import { theme } from './theme'
 import Table from './components/AdvancedTable'
 import Layout from './components/Layout'
+import { VdsTabs, type VdsTabConfig } from './components/Vds/Tabs/VdsTabs'
+import { getSelectedElement } from './components/Vds/Tabs/VdsTabsUtils'
+
+const tabsConfig: VdsTabConfig[] = [
+  { id: 'advanced-table', label: 'Advanced Table', selected: true },
+  { id: 'dummy', label: 'Element' },
+]
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState(getSelectedElement(tabsConfig))
+  const handleTabSelect = (tab: VdsTabConfig) => {
+    setSelectedTab(tab)
+  }
+
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
       <Layout>
-        <Table />
+        <VdsTabs onSelection={handleTabSelect} config={tabsConfig} />
+        {selectedTab?.id === 'advanced-table' && <Table />}
       </Layout>
     </MantineProvider>
   )
