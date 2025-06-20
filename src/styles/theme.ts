7adc03612aa0d89479f57f770e28bc6ca4082506
ZowWeb <zohaib.khan@in.verizon.com>
2025-06-20 12:19:31 +0530

    Fix fonts, font sizes, breakpoints etc

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,23 @@ import { type MantineThemeOverride } from '@mantine/core'
 
 export const theme: MantineThemeOverride = {
   colorScheme: 'light',
+  fontFamily:
+    '"Verizon-NHG-eDS", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+  fontSizes: {
+    xs: '0.625rem', // 10px
+    sm: '0.75rem', // 12px
+    md: '1rem', // 16px
+    lg: '2rem', // 32px
+    xl: '3rem', // 48px
+    '2xl': '4rem', // 64px
+  },
+  breakpoints: {
+    xs: '30rem', // 480px
+    sm: '48rem', // 768px
+    md: '68.25rem', // 1092px
+    lg: '80rem', // 1280px
+    xl: '90rem', // 1440px
+  },
   components: {
     AppShell: {
       styles: {
@@ -10,19 +27,5 @@ export const theme: MantineThemeOverride = {
         main: { padding: 0, margin: 0 },
       },
     },
-  },
-  fontSizes: {
-    xs: '0.75rem', // 12px
-    sm: '0.875rem', // 14px
-    md: '1rem', // 16px
-    lg: '1.125rem', // 18px
-    xl: '1.25rem', // 20px
-  },
-  breakpoints: {
-    xs: '30rem', // 480px
-    sm: '48rem', // 768px
-    md: '68.25rem', // 1092px
-    lg: '80rem', // 1280px
-    xl: '90rem', // 1440px
   },
 }
