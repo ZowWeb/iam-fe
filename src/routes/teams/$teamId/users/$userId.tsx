40c2501a9d650532c2f6adaea76363d52c38ae53
ZowWeb <zohaib.khan@in.verizon.com>
2025-07-03 17:15:25 +0530

    Member Details page few changes

--- src/routes/teams/$teamId/users/$userId.tsx
+++ src/routes/teams/$teamId/users/$userId.tsx
@@ -1,7 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import Typography from '~/components/Typography'
+
 function MemberPage() {
-  return <div>Hello, this is Member Details page!</div>
+  return (
+    <div>
+      <Typography.H1>Hello World!</Typography.H1>
+      <Typography.H3>this is Member Details page!</Typography.H3>
+    </div>
+  )
 }
 
 export const Route = createFileRoute('/teams/$teamId/users/$userId')({
