021bec84aaa298b09fb9d83223da726ecbf8435d
ZowWeb <zohaib.khan@in.verizon.com>
2025-07-10 14:19:37 +0530

    Update member details page

---
+++ src/pages/MemberPage.tsx
@@ -0,0 +1,41 @@
+import { IconChevronLeft } from '@tabler/icons-react'
+
+import Block from '~/components/Block'
+import IamHero from '~/components/IamHero'
+import { VdsTabs, type VdsTabConfig } from '~/components/Vds/Tabs/VdsTabs'
+import Grid, { Col } from '~/components/Grid'
+import FlexBox from '~/components/FlexBox'
+import Typography from '~/components/Typography'
+import { COLORS } from '~/styles/constants'
+
+const tabsConfig: VdsTabConfig[] = [
+  { id: 'teamDetails', label: 'Team Details' },
+  { id: 'members', label: 'Members', selected: true },
+  { id: 'policies', label: 'Policies' },
+  { id: 'serviceAccounts', label: 'Service accounts' },
+]
+
+const MembersPage = () => {
+  return (
+    <Block>
+      <Grid>
+        <Col span={3}>
+          <VdsTabs onSelection={() => {}} config={tabsConfig} />
+        </Col>
+        <Col span={9}>
+          <FlexBox customStyle={{ marginBottom: '2rem' }} gap="3">
+            <IconChevronLeft name="arrow-left" size={20} />
+            <span>Back to member list</span>
+          </FlexBox>
+          <Typography.H3 style={{ color: COLORS.vdsRed, marginBottom: '0.5rem' }}>
+            Member details
+          </Typography.H3>
+          <Typography.Paragraph>View details of this member and manage their access. </Typography.Paragraph>
+          <IamHero />
+        </Col>
+      </Grid>
+    </Block>
+  )
+}
+
+export default MembersPage
--- src/routes/teams/$teamId/users/$userId.tsx
+++ src/routes/teams/$teamId/users/$userId.tsx
@@ -1,15 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-import Typography from '~/components/Typography'
-
-function MemberPage() {
-  return (
-    <div>
-      <Typography.H1>Hello World!</Typography.H1>
-      <Typography.H3>this is Member Details page!</Typography.H3>
-    </div>
-  )
-}
+import MemberPage from '~/pages/MemberPage'
 
 export const Route = createFileRoute('/teams/$teamId/users/$userId')({
   component: MemberPage,
