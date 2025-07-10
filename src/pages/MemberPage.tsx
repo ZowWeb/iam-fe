d7a2876319862b99c88bd153fc0180087717baf2
ZowWeb <zohaib.khan@in.verizon.com>
2025-07-10 15:18:38 +0530

    Use spacings and colors from theme

--- src/pages/MemberPage.tsx
+++ src/pages/MemberPage.tsx
@@ -6,7 +6,6 @@ import { VdsTabs, type VdsTabConfig } from '~/components/Vds/Tabs/VdsTabs'
 import Grid, { Col } from '~/components/Grid'
 import FlexBox from '~/components/FlexBox'
 import Typography from '~/components/Typography'
-import { COLORS } from '~/styles/constants'
 
 const tabsConfig: VdsTabConfig[] = [
   { id: 'teamDetails', label: 'Team Details' },
@@ -23,11 +22,11 @@ const MembersPage = () => {
           <VdsTabs onSelection={() => {}} config={tabsConfig} />
         </Col>
         <Col span={9}>
-          <FlexBox customStyle={{ marginBottom: '2rem' }} gap="3">
+          <FlexBox customStyle={{ marginBottom: '2rem' }}>
             <IconChevronLeft name="arrow-left" size={20} />
             <span>Back to member list</span>
           </FlexBox>
-          <Typography.H3 style={{ color: COLORS.vdsRed, marginBottom: '0.5rem' }}>
+          <Typography.H3 mb="md" c="vdsRed">
             Member details
           </Typography.H3>
           <Typography.Paragraph>View details of this member and manage their access. </Typography.Paragraph>
