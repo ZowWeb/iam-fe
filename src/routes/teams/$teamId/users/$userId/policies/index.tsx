ea8b52a011dc8b4d27187a197ef6dda2964edf2d
ZowWeb <zohaib.khan@in.verizon.com>
2025-08-06 14:29:40 +0530

    Rename page to follow same convention

--- src/routes/teams/$teamId/users/$userId/policies/index.tsx
+++ src/routes/teams/$teamId/users/$userId/policies/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-import Page from '~/pages/MemberPolicies'
+import Page from '~/pages/MemberPoliciesPage'
 
 export const Route = createFileRoute('/teams/$teamId/users/$userId/policies/')({
   component: Page,
