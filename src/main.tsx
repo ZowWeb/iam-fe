fed0653f60a42ff60ef1c2052d0aad2b379f6367
Sean Edwards <sean.edwards@verizon.com>
2025-08-11 17:00:29 -0400

    Change base paths

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ import reportWebVitals from './reportWebVitals'
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, basepath: '/iam' })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
