df6ec6b6e91b34016e4d5ccf30eef1ba23c5b8a6
ZowWeb <zohaib.khan@in.verizon.com>
2025-08-12 17:15:21 +0530

    Refactor useMembers hook

--- src/hooks/useMembers.ts
+++ src/hooks/useMembers.ts
@@ -1,23 +1,12 @@
 import { useQuery } from '@tanstack/react-query'
 
 import { getMembers } from '~/queries/getMembers'
-import type { Member } from '~/types/data'
 
 export default function useMembers() {
-  const { data: members, isLoading, isError } = useQuery(getMembers)
-
-  if (isError || !members) {
-    console.error('Error fetching members or no members found')
-    return {
-      members: [] as Member[],
-      isLoading,
-      isError: true,
-    }
-  }
+  const { data: members = [], isLoading } = useQuery(getMembers)
 
   return {
     members,
     isLoading,
-    isError: false,
   }
 }
