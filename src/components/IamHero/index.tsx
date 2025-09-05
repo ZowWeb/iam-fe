fd6c8dad6e85dee534b1eb0b3d874dda56244e62
Esteban Mariano <esteban.piga.alessi@verizon.com>
2025-09-05 09:53:54 -0300

    Fix hero action  button style when clicked

--- src/components/IamHero/index.tsx
+++ src/components/IamHero/index.tsx
@@ -1,4 +1,5 @@
 import { ButtonIcon } from '@vds/button-icons'
+import { Icon, type IconProps } from '@vds/icons'
 
 import { Wrapper, TitleWithActionWrapper, Title, Subtitle } from './styles'
 
@@ -20,25 +21,7 @@ const IamHero = ({ title, subtitle, showActionButton = false, children, gap = '1
             surfaceType="colorFill"
             kind="lowContrast"
             size="large"
-            renderIcon={() => (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={24}
-                height={24}
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-dots"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-                <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-                <path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-              </svg>
-            )}
+            renderIcon={(props: IconProps) => <Icon name="more-horizontal" {...props} />}
             onClick={() => {}}
           />
         )}
