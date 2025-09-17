16e2ac2a64f6b751186f91e2f2a4954ee848625f
ZowWeb <zohaib.khan@in.verizon.com>
2025-09-17 15:44:13 +0530

    refactor: API call should be on landing page not route

--- src/routes/_authenticated/teams/$teamId/roles/index.tsx
+++ src/routes/_authenticated/teams/$teamId/roles/index.tsx
@@ -1,7 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 import RolesPage from '~/pages/RolesPage'
+import getPolicyTags from '~/queries/getPolicyTags'
 
 export const Route = createFileRoute('/_authenticated/teams/$teamId/roles/')({
   component: RolesPage,
+  loader: async ({ context: { queryClient }, params: { teamId } }) => {
+    return {
+      policyTags: await queryClient.ensureQueryData(getPolicyTags({ teamId })),
+    }
+  },
 })
--- src/routes/_authenticated/teams/$teamId/roles/route.tsx
+++ src/routes/_authenticated/teams/$teamId/roles/route.tsx
@@ -1,15 +1,10 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
 
-import getPolicyTags from '~/queries/getPolicyTags'
-
 const RouteComponent = () => <Outlet />
 
 export const Route = createFileRoute('/_authenticated/teams/$teamId/roles')({
   component: RouteComponent,
-  loader: async ({ context: { queryClient }, params: { teamId } }) => {
-    return {
-      policyTags: await queryClient.ensureQueryData(getPolicyTags({ teamId })),
-      crumbTitle: 'Roles',
-    }
-  },
+  loader: () => ({
+    crumbTitle: 'Roles',
+  }),
 })
