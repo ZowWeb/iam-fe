7a1e79a6e1a54f909c62f323070cc83ec5bc9f16
Esteban Mariano <esteban.piga.alessi@verizon.com>
2025-09-19 09:29:01 -0300

    Rebase fixes

--- src/routes/_authenticated/teams/$teamId/users/index.tsx
+++ src/routes/_authenticated/teams/$teamId/users/index.tsx
@@ -5,7 +5,7 @@ import getTeamMembers from '~/queries/getTeamMembers'
 
 export const Route = createFileRoute('/_authenticated/teams/$teamId/users/')({
   component: MembersPage,
-  loader: async ({ context: { queryClient } }) => {
+  loader: async ({ context: { queryClient }, params: { teamId } }) => {
     return {
       members: await queryClient.ensureQueryData(getTeamMembers({ teamId })),
     }
