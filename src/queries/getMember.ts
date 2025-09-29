c6a105992757c32c3bfb0344b10288419413683b
ZowWeb <zohaib.khan@in.verizon.com>
2025-09-29 18:11:38 +0530

    refactor: Use apiServerWithThrow in getMember

--- src/queries/getMember.ts
+++ src/queries/getMember.ts
@@ -1,26 +1,18 @@
 import { queryOptions } from '@tanstack/react-query'
 
 import type { Member } from '~/types/data'
-import { handleErrorMessage } from '~/utils/errors'
+import apiServerWithThrow from '~/utils/apiServerWithThrow'
 
 type GetMemberProps = {
   userId: string
 }
 
 export const getMember = ({ userId }: GetMemberProps) =>
-  queryOptions<Member | undefined>({
+  queryOptions<Member>({
     queryKey: ['GET_MEMBER', { userId }],
     queryFn: async () => {
-      try {
-        const response = await fetch(`https://iamservice.dev.api.aws.tpd-soe.net/users/${userId}`)
-        if (!response.ok) {
-          throw new Error(
-            `[getMember] Network response was not ok! [res]: ${response.status} ${response.statusText}`,
-          )
-        }
-        return response.json()
-      } catch (error) {
-        console.error(`[getMember] Error fetching member:`, handleErrorMessage(error))
-      }
+      const response = await apiServerWithThrow({ endpoint: `/users/${userId}` })
+
+      return response.json()
     },
   })
