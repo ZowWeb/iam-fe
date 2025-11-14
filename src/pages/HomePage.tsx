cca8e8115e23c8b4ea7eb117418f2a62c293bf70
Midun Mano M <midun.mano.m@in.verizon.com>
2025-11-14 12:07:01 +0530

    changes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -24,7 +24,7 @@ export default function HomePage() {
       console.info('No data found, redirecting to initiate PKCE login')
       window.location.href = 'https://console.apideveloper-dev.verizon.com/auth/login'
     }
-  }, [result, navigate])
+  }, [result, setIsAuthenticated])
 
   return (
     <Layout>
